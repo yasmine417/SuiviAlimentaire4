70bebf94a6f71ccc1f21dd494010b2201b6c49a3
Yasmine Benomar <153392423+yasmine417@users.noreply.github.com>
2025-12-25 12:37:33 +0100

    Update

--- SuiviAlimentaire1/src/app/core/services/ocr-ia.service.ts
+++ SuiviAlimentaire1/src/app/core/services/ocr-ia.service.ts
@@ -15,21 +15,21 @@ export interface IaResult {
 })
 export class OcrIaService {
 
-  // Microservice OCR
+  
   private ocrUrl = 'http://localhost:8082/api/ocr/upload';
-  // Microservice IA
+  
   private iaUrl = 'http://localhost:8084/api/calories';
 
   constructor(private http: HttpClient) {}
 
-  // Upload de l'image pour OCR
+  
   uploadImage(file: File): Observable<{mealName: string, ocrText: string}> {
     const formData = new FormData();
     formData.append('image', file); // le nom "image" doit correspondre au @RequestParam côté Spring Boot
     return this.http.post<{mealName: string, ocrText: string}>(this.ocrUrl, formData);
   }
 
-  // Calcul des calories après OCR
+  
   calculateCalories(mealName: string, ocrText: string): Observable<IaResult> {
     return this.http.post<IaResult>(this.iaUrl, { mealName, ocrText });
   }
